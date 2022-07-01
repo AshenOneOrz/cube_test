434f21b052b991f4349205804d99404f7e3db83b
ashenoneorz <ashenoneorz@gmail.com>
2022-07-01 11:43:12 +0800

    init

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+    import Main from './components/Main.svelte'
+</script>
+
+<Main></Main>
+
+<style>
+    @tailwind base;
+    @tailwind components;
+    @tailwind utilities;
+</style>
---
+++ src/components/Main.svelte
@@ -0,0 +1,3 @@
+<main>
+    <h1 class="text-red-500 text-center">Hello Svelte</h1>
+</main>
